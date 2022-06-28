43b040fae629de321cc72d2f75866edfa4dcfa8b
Rodrigo Tozatti <rodrigo.tozatti@hotmail.com>
2022-06-27 22:40:11 -0300

    altrações pre-cadastro e produtos

--- cypress/e2e/Pre-cadastro.spec.cy.js
+++ cypress/e2e/Pre-cadastro.spec.cy.js
@@ -9,14 +9,18 @@ describe('Funcionalidade Pré Cadastro', () => {
   })
 
   it('deve completar o pre cadastro com sucesso', () => {
+
+    var nomefaker = faker.name.firstName()
+    var sobrenomefaker = faker.name.lastName()
+    var emailfaker = faker.internet.email(nomefaker)
     
-    cy.get('#reg_email').type(faker.internet.email())
+    cy.get('#reg_email').type(emailfaker)
     cy.get('#reg_password').type('teste_06@teste.com')
     cy.get(':nth-child(4) > .button').click()
 
     cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-    cy.get('#account_first_name').type(faker.name.firstName())
-    cy.get('#account_last_name').type(faker.name.lastName())
+    cy.get('#account_first_name').type(nomefaker)
+    cy.get('#account_last_name').type(sobrenomefaker)
     cy.get('.woocommerce-Button').click()
 
     cy.get('.woocommerce-message').should('contain', ('Detalhes da conta modificados com sucesso.'))
