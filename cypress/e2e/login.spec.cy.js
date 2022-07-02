06f1850b5a282c42cecc4c4b4f5ea25410c953dc
Rodrigo Tozatti <rodrigo.tozatti@hotmail.com>
2022-07-02 00:51:17 -0300

    pastas atualizadas

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -3,7 +3,7 @@
 context('Funcionalidade login', () =>{
 
   it('Deve fazer login com sucesso', () =>{
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.visit('minha-conta/')
     cy.get('#username').type('aluno_ebac@teste.com')
     cy.get('#password').type('teste@teste.com')
     cy.get('.woocommerce-form > .button').click()
@@ -14,7 +14,7 @@ context('Funcionalidade login', () =>{
   }) 
 
   it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () =>{
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.visit('minha-conta/')
     cy.get('#username').type('ebac@teste.com')
     cy.get('#password').type('teste@teste.com')
     cy.get('.woocommerce-form > .button').click()
@@ -24,7 +24,7 @@ context('Funcionalidade login', () =>{
   })
 
   it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.visit('minha-conta/')
     cy.get('#username').type('aluno_ebac@teste.com')
     cy.get('#password').type('teste@teste')
     cy.get('.woocommerce-form > .button').click()
